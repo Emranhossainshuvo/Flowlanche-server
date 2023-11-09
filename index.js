644e1eabe01443172fb0eef37150daf99a04f049
Shuvo <shuvoorba28@gmail.com>
2023-11-08 16:25:20 -0800

    update job functionality added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleqwares
@@ -12,10 +12,9 @@ app.use(express.json());
 // console.log(process.env.DB_PASS)
 // console.log(process.env.DB_USER)
 
-// configuaration from mongodb  
+// configuaration from mongodb
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,64 +31,87 @@ async function run() {
     await client.connect();
 
     // database  and collection name
-    const jobCollection = client.db('FlowLancher').collection('jobs')
-    const usersCollection = client.db('FlowLancher').collection('user')
+    const jobCollection = client.db("FlowLancher").collection("jobs");
+    const usersCollection = client.db("FlowLancher").collection("user");
 
     // find all jobs from mongodb
-    app.get('/jobs', async(req, res) => {
-        const cursor = jobCollection.find();
-        const result = await cursor.toArray(); 
-        res.send(result); 
-    })
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    // find a specific job for click on bid now button 
+    // find a specific job for click on bid now button
 
-    app.get('/jobs', async(req, res) => {
-      console.log(req.query)
-      const result = await jobCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/jobs", async (req, res) => {
+      console.log(req.query);
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
 
-    
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/jobs/:id', async(req, res) => {
-      const id = req.params.id; 
-      const query = {_id: new ObjectId(id)}
-      const result = await jobCollection.findOne(query)
-      res.send(result)
-    } )
-
-    
-
-// post jobs to database
-    app.post('/jobs', async(req, res) => {
-        const newJobs = req.body; 
-        console.log(newJobs)
-        const result = await jobCollection.insertOne(newJobs); 
-        res.send(result)
-    })
-
+    // post jobs to database
+    app.post("/jobs", async (req, res) => {
+      const newJobs = req.body;
+      console.log(newJobs);
+      const result = await jobCollection.insertOne(newJobs);
+      res.send(result);
+    });
 
     // getting some data to show in the my my posted jobs
 
-    app.get('/jobs', async(req, res) => {
-      console.log(req.query.email)
-      let query = {}
-      if(req.query?.email){
-        query = {email: req.query.email}
+    app.get("/jobs", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
-      const result = await jobCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // api for deleting an specific job from an specific user
 
-    app.delete('/jobs/:id', async(req, res) => {
-      const id = req.params.id; 
-      const query = {_id: new ObjectId(id)}
-      const result = await jobCollection.deleteOne(query); 
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // api for get specific job's data to update the data
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    // api for update  a specific job
+    app.put("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedJob = req.body;
+      const job = {
+        $set: {
+          title: updatedJob.title,
+          description: updatedJob.description,
+          maximum: updatedJob.maximum,
+          minimum: updatedJob.minimum,
+          deadline: updatedJob.deadline,
+          select: updatedJob.select
+        }
+      };
+      const result = await jobCollection.updateOne(filter, job, options)
       res.send(result)
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
